3d2dff56e19c5a36b978f883b858211ca79bc2e7
naved574 <navedansari2543@gmail.com>
2026-07-06 23:18:16 +0530

    removing waist parts

--- src/app/api/_lib/db.ts
+++
@@ -1,8 +0,0 @@
-let connection: Promise<void> | null = null;
-
-export async function ensureMongo() {
-  if (!connection) {
-    connection = import("../../../../backend/dist/db/mongoose.js").then(({ connectMongo }) => connectMongo());
-  }
-  return connection;
-}
